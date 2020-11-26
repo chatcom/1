a78c0130ce16e6a3695cacef5520ab186f3c22e7
chatcom <75067898+chatcom@users.noreply.github.com>
2020-11-26 12:59:04 +0400

    Create 5o5h.js

---
+++ 5o5h.js
@@ -0,0 +1,34 @@
+$(`<img src=" sico/1583097699187.jpg "gclass="fr" style="width:100%;margin-right: 2px;">
+<a href="https://chatcom.live/" style="width:90px;margin:2px;" class="fl mini  fa   fa-phone btn btn-warning"> جنون العاشقين  </a>
+<a href="https://chatcom.live/" style="width:85px;margin:2px;" class="fl mini  fa  fa-heartbeat
+btn btn-danger"> التطبيـق</a>
+<a href="https://chatcom.live/" style="width:87px;margin:2px;" class="fl mini  fa   fa-star
+btn btn-success">واتس اب</a>
+<a href="https://chatcom.live/" style="width:90px;margin:2px;" class="fl mini  fa fa-graduation-cap
+btn btn-info"> القوانين</
+<div style=" width: 100%; " id="asim33" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"> اداره الموقع </div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary">  <i class="fa fa-paper-plane">  </i> اداره 
+الموقع </p> <i class="fa fa-comment"></i> <i 
+class="fa fa-share"> </i> </p> <i class="fa fa-comment"></i> <i class="fa fa-share"> خـــوخـــه </i> </p> <i class="fa fa fa-comment"> </i> <i class="fa fa-share"> </i> </div></div><center></div></div><center><center><div><marquee direction="right" width="99%" id="asim7" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="2" >
+
+`).insertBefore('.nav-tabs');
+
+$('.nav-tabs').addClass('fl').css('width','100%');
+
+
+if(location.protocol!='https:'){location.protocol='https:';}
+
+$(`<a href="https://coolnames.online" target="_blank" style=" font-family: 'El Messiri', sans-serif;width:107px;color: #E8415D;text-decoration:none; margin-right: 5px;
+margin-left: 2px;text-align: center;float: right;border-radius: 5px;
+border-color: #F2F2F2;
+padding: 3px;" class="border" ><i class="fa fa-user"></i>&nbsp;زخرف أسمك</a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
+$(`<center><font size="4" color="#FFFFFF"> <marquee direction="right" width="100%" height="20" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5" style="height: 20px; width: 100%;">  اخي الزائر ... اُختي الزائره.. أهلاً وسهلاً بكم في (شات جنون العاشقين للجوال) ونفيدكم علماً بانه قد تم نقل مِلكية الموقع إلى الاخت (اوجاع الشمري) ومن حقها تاخذ كل الإحترازات والإجرآءت اللآزمه لذالك قمنا بحذف جميع المجموعات + الإشتراكات  وحين يكون هناك  تفاعل من قِبلكُم سنُعيد إليكم صلاحياتكم التي انسحبت منكم ولكم  جميل الود والإحترام 
+
+‪    </marquee></font><center>`).insertBefore('#d2');
+
+
+if(location.protocol!='https:'){location.protocol='https:';}
+
+$(`<a href="https://www.youtube.com" target="_blank" style=" font-family: 'El Messiri', sans-serif;width:107px;color: #E8415D;text-decoration:none; margin-right: 5px;
+margin-left: 2px;text-align: center;float: right;border-radius: 5px;
+border-color: #F2F2F2;
+padding: 3px;" class="border" ><i class="fa fa-user"></i>&nbsp;YouTube</a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
